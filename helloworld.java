93527cacfb6875b2fba321c2a78da9d692b87f88
byreswara <byreswarareddy.k@gmail.com>
2020-10-15 12:05:09 +0530

    conflicts ressolved

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,6 @@
 class helloworld {
  public static void main(String args[]){
 
-     System.out.println("Hello World hcl");
-
-     System.out.println("Hello World welcome");
-
+     System.out.println("Hello World");
  }
 }
